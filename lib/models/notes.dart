e07c899cd646381e21bc9250b3b51f5c7c9dbfcc
a-tiny-developer <atinyflame@gmail.com>
2022-01-03 14:36:29 -0600

    Models, class notes

---
+++ lib/models/notes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Notes {
+  static final List<_Note> notes = [
+    _Note(color: Colors.deepPurple[50]!, name: 'note1.wav'),
+    _Note(color: Colors.deepPurple[100]!, name: 'note2.wav'),
+    _Note(color: Colors.deepPurple[200]!, name: 'note3.wav'),
+    _Note(color: Colors.deepPurple[400]!, name: 'note4.wav'),
+    const _Note(color: Colors.deepPurple, name: 'note5.wav'),
+    _Note(color: Colors.deepPurple[700]!, name: 'note6.wav'),
+    _Note(color: Colors.deepPurple[900]!, name: 'note7.wav'),
+  ];
+}
+
+class _Note {
+  final Color color;
+  final String name;
+
+  const _Note({
+    required this.color,
+    required this.name,
+  });
+}
